502ecf10cb9dcec1a442bf1858676bf503a2c60d
Caio Lima <caio.lima@justdigital.com.br>
2017-03-28 12:01:55 -0300

    Arrumando textos do similar tet que estavam em portugues

--- strings-patterns/code/similar-text.php
+++ strings-patterns/code/similar-text.php
@@ -1,8 +1,7 @@
 <?php
 //similar_text - int similar_text ( string $first , string $second [, float &$percent ] )
-$string1  = 'Av. Livro de  Certificação PHP';
-$string2  = 'Rua, Certificação PHP';
+$string1  = 'Book of Certification PHP';
+$string2  = 'Certification PHP';
 print similar_text($string1,  $string2, $percent);
 echo PHP_EOL;
 print $percent;
-print levenshtein($string1, $string2);
