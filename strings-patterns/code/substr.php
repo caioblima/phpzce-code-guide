7f581ab2e3b68581565ab179b8e77f483a164133
Caio Lima <caio.lima@justdigital.com.br>
2017-03-28 12:12:12 -0300

    ajustando substr

--- strings-patterns/code/substr.php
+++ strings-patterns/code/substr.php
@@ -1,5 +1,5 @@
 <?php
-//substr pegar porção de string como init e limit
-$codigo = 'A3-99812.FFGD';
-print substr($codigo, 0,  3); //  A3-
+//substr
+$code = 'A3-99812.FFGD';
+print substr($code, 0,  3); //  A3-
 echo substr('abcdef', -4, -2); //cd
