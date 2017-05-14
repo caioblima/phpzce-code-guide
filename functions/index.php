805dabb3961f6bef6cd820d9bbd3c78b234322ae
Caio Lima <caio_liima@live.com>
2017-05-14 20:58:29 -0300

    Starting functions

---
+++ functions/index.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+// $zceStr = 'Zend Certified Engineer';
+// function toUpper(&$str) {
+//   $str = mb_strtoupper($str);
+// }
+// toUpper($zceStr);
+// print_r($zceStr);
+
+// function convertDolarToBRL($amount, $dolar = 3.10) {
+//   return number_format($amount * $dolar, 2, '.', ',');
+// }
+// print_r(convertDolarToBRL(790));
+// function sumValues($a, $b) {
+//   return $a + $b;
+// }
+// $sumSomeValues = sumValues(2, 4);
+$f1Teams = [];
+function ourArrayPush(array &$array, $data) {
+  if ($data !== null) {
+    $array[] = $data;
+  }
+}
+print_r(ourArrayPush($f1Teams, 'Ferrari'));
+print_r($f1Teams);
